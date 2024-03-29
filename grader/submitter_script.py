8bc784bf2c771e3a8428bfc87dd40cf6028273fc
rcmurray <>
2024-03-29 17:52:40 -0400

    Try to reconcile with Sail-v2 submitter from ope-problem-representation

--- grader/submitter_script.py
+++ grader/submitter_script.py
@@ -1,24 +1,25 @@
 import requests, json, os
 import tarfile
 from operator import itemgetter
-from test_utility import write_result
+from test_utility import write_encrypted_result
 
 # Submission Specific Config
 LMS_NAME = "sail2"
-PROJECT_ID = "ope-phase-2"
-
-TASK_ID = "mysql-ope-task"
-SECRET_KEY = "Md7FnrQTwY4rEqxSSCrU28"
+PROJECT_ID = "ope-cloud-admin"    						// WHAT SHOULD VALUE BE? 
+PROJECT_LEARN_ID = "ope-learn-autoscalin-mpfs4jua"    	// WHAT SHOULD VALUE BE?
+TASK_ID = "ope-cloud-admin"    							// WHAT SHOULD VALUE BE? 
+SECRET_KEY = "Md7FnrQTwY4rEqxSSCrU28"    				// WHAT SHOULD VALUE BE? 
 ARTIFACT_VERSION = "v1"
-DURATION = 300
-COURSE_TYPE = "cloud-developer"
+DURATION = 300    										// WHAT SHOULD VALUE BE? 
+COURSE_TYPE = "cloud-admin"    							// WHAT SHOULD VALUE BE? 
 
 STUDENT_DNS = requests.get("https://ipinfo.io/ip").text
 AGS_DNS = "autograding.sailplatform.org"
 SIGNATURE = "1K9SaGliHwthRgeOi12hUdCUwAPmN"
 
 NOTEBOOK_FILENAME = "./workspace/workspace.ipynb"
-RESULT_FILENAME = "task_result.json"
+# RESULT_FILENAME = "task_result.json"
+RESULT_FILENAME = "result.json"
 
 
 def submit(username, password, result):
@@ -39,12 +40,12 @@ def submit(username, password, result):
 	tar_filename = f"{username}.tar.gz"
 
 	# generate the result json file
-	write_result(result, RESULT_FILENAME)
+	write_encrypted_result(result, RESULT_FILENAME)
 
 	# add the result json file and workspace notebook
 	# to a submission tar
 	with tarfile.open(tar_filename, "w:gz") as tar:
-		tar.add('solutions/', recursive=True)
+		# tar.add('solutions/', recursive=True)
 		tar.add(NOTEBOOK_FILENAME)
 		tar.add(RESULT_FILENAME)
 
